28d434b38be823bed3d7b195a4eaddea29e041ab
Christopher <Chris.97e@gmail.com>
2019-05-01 23:19:08 -0500

    navBar added

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-var express = require('express');
+var express = require('express'),
+engines = require('consolidate'),
+handlebars = require('handlebars');
+
 var app = express();
 
+app.engine('hbs', engines.handlebars);
+app.set ('views', './views');
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
-    res.send('Hola Mundo!');
+    res.render('home', {
+        title: 'mis ',
+        
+    });
 });
 
-
-app.get('/section', function(req, res) {
-    res.send('Seccion');
-});
 
 app.listen(3000, function() {
     console.log('Aplicación ejemplo, escuchando el puerto 3000!');
