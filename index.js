956047200bfa39968ffd82a296dfae4531088bec
Christopher <Chris.97e@gmail.com>
2019-05-05 23:33:16 -0500

    conecction database update, no more with Moongose

--- index.js
+++ index.js
@@ -1,45 +1,65 @@
 var express = require('express'),
 engines = require('consolidate'),
 handlebars = require('handlebars');
-
+const MongoClient = require('mongodb').MongoClient;
 var app = express();
 
 app.engine('hbs', engines.handlebars);
-app.set ('views', './views');
+app.set('views', './views');
 app.set('view engine', 'hbs');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 
-const mongoose = require('mongoose');
+
 
 // This is the conecction with mongodb 
 // Here I'm telling to mongodb that I want to conect specifically to the cumIn database
-mongoose.connect('mongodb://localhost/cumIn');
+const url = 'mongodb://localhost:27017';
+const dbName = 'cumIn';
+const client = new MongoClient(url);
+var db = null;
+
+
 
 // This is for know when te conecction is ready, we only want to know once, thats te reason why
 // we use "once" on the event
 
-mongoose.connection.once('open', function () {
 
-    console.log("Conection has been made!");
-
-}).on('error', function (error) {
-    console.log("conecction error:", error);
-
+client.connect(function (err) {
+    assert.equal(null, err);
+    
+    db = client.db(dbName);
+    console.log("Conecction has been made!");
+    //client.close();
 });
 
 
 
-app.get('/', function(req, res) {
+
+
+
+app.get('/', function (req, res) {
     res.render('home');
 });
 
-app.get('/store', function(req, res) {
-    res.render('store');
+app.get('/store', function (req, res) {
+    
+    //Accedemos a la conección
+    var collection = db.collection('products');
+    collection.find(query).toArray(function (err, docs) {
+        assert.equal(err, null);
+        
+        var contexto = {
+            products: docs,
+            
+        };
+        response.render('store', contexto);
+    });
 });
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log('Aplicación ejemplo, escuchando el puerto 3000!');
 });
